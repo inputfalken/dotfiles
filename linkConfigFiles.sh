425a71d666e8377e5a80479d21782e5a52468d98
Robert Andersson <inputfalken@gmail.com>
2017-03-31 00:15:50 +0200

    Add script to link dotfiles

---
+++ linkConfigFiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#TODO Create a proper install file
+link ./.gitconfig ~/.gitconfig
+link ./.vimrc ~/.vimrc
+link ./.vimrc.plugins ~/.vimrc.plugins
+link ./.gitignore_global ~/.gitignore_global
+link ./.tmux.conf ~/.tmux.conf
+link ./.vimrc.syntastic ~/.gitconfig
+link ./.tern-project ~/.tern-project
