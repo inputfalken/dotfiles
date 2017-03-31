b29531db308ba8b22db5734b05e7cee83c8574ab
Robert Andersson <inputfalken@gmail.com>
2017-03-31 21:05:54 +0200

    Add script to check os

--- linkConfigFiles.sh
+++ linkConfigFiles.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 #TODO Create a proper install file
+if [ "$(uname)" == "Darwin" ]; then
+  echo 'Mac osx'
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  echo 'Linux'
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+  echo 'Windows 32 bit'
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+  echo 'Windows 64 bit'
+fi
 link ./.gitconfig ~/.gitconfig
 link ./.vimrc ~/.vimrc
 link ./.vimrc.plugins ~/.vimrc.plugins
