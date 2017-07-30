3e395ff8790d9b45e8874780156e0a194ca3b0d7
Robert Andersson <inputfalken@gmail.com>
2017-07-30 23:15:32 +0200

    Add posh-git module

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,3 @@
+Import-Module posh-git
 # Source: https://github.com/neilpa/cmd-colors-solarized#update-your-powershell-profile
 . (Join-Path -Path (Split-Path -Parent -Path $PROFILE) -ChildPath $(switch($HOST.UI.RawUI.BackgroundColor.ToString()){"White"{"Set-SolarizedLightColorDefaults.ps1"}"Black"{"Set-SolarizedDarkColorDefaults.ps1"}default{return}}))
