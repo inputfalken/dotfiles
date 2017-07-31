c6898ed0235def137905cf4adac0dbcc23556dca
RobertOfferta <robert.andersson@offerta.se>
2017-07-31 17:40:58 +0200

    Add reload path function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,8 @@
 Import-Module posh-git
 # Source: https://github.com/neilpa/cmd-colors-solarized#update-your-powershell-profile
 . (Join-Path -Path (Split-Path -Parent -Path $PROFILE) -ChildPath $(switch($HOST.UI.RawUI.BackgroundColor.ToString()){"White"{"Set-SolarizedLightColorDefaults.ps1"}"Black"{"Set-SolarizedDarkColorDefaults.ps1"}default{return}}))
+
+# Reload the system path variable.
+function Reload-Path {
+  $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")       
+}
