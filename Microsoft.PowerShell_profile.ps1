c468e0f05c722459af173750fdb0fbd81fdadf84
Robert Andersson <robert.andersson@offerta.se>
2017-08-02 15:51:27 +0200

    Add Exec function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,7 +5,7 @@ Set-PSReadlineOption -EditMode vi
 
 # Reload the system path variable.
 function Reload-Path {
-  $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")       
+  $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 }
 
 function Create-Link ($target, $link) {
@@ -13,3 +13,57 @@ function Create-Link ($target, $link) {
 }
 
 Set-Alias vi vim
+
+<#
+.SYNOPSIS
+This is a helper function that runs a scriptblock and checks the PS variable $lastexitcode
+to see if an error occcured. If an error is detected then an exception is thrown.
+This function allows you to run command-line programs without having to
+explicitly check the $lastexitcode variable.
+
+.EXAMPLE
+exec { svn info $repository_trunk } "Error executing SVN. Please verify SVN command-line client is installed"
+#>
+
+function Exec {
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
+        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd),
+        [Parameter(Position=2,Mandatory=0)][int]$maxRetries = 0,
+        [Parameter(Position=3,Mandatory=0)][string]$retryTriggerErrorPattern = $null
+    )
+
+    $tryCount = 1
+
+    do {
+        try {
+            $global:lastexitcode = 0
+            & $cmd
+            if ($lastexitcode -ne 0) {
+                throw ("Exec: " + $errorMessage)
+            }
+            break
+        }
+        catch [Exception] {
+            if ($tryCount -gt $maxRetries) {
+                throw $_
+            }
+
+            if ($retryTriggerErrorPattern -ne $null) {
+                $isMatch = [regex]::IsMatch($_.Exception.Message, $retryTriggerErrorPattern)
+
+                if ($isMatch -eq $false) {
+                    throw $_
+                }
+            }
+
+            Write-Host "Try $tryCount failed, retrying again in 1 second..."
+
+            $tryCount++
+
+            [System.Threading.Thread]::Sleep([System.TimeSpan]::FromSeconds(1))
+        }
+    }
+    while ($true)
+}
