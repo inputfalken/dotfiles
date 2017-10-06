64f44424e112a6083234f4eec8ad89d9bb447099
Robert Andersson <robert.andersson@offerta.se>
2017-10-06 14:50:31 +0200

    Clean up script

--- powershell/scripts/removeMicrosoftDefaultPrograms.ps1
+++ powershell/scripts/removeMicrosoftDefaultPrograms.ps1
@@ -1,29 +1,9 @@
+[CmdletBinding(SupportsShouldProcess=$True)]
+param()
 function Remove-WindowsApps {
-  [CmdletBinding(SupportsShouldProcess=$True)]
-  param([string[]] $whiteList)
-   @('messaging', 'windowsalarms', 'windowscommunicationsapps', 'windowscamera', 'sway', 'commsphone', 'windowsphone', 'phone', 'communicationsapps', 'people', 'zunemusic', 'zunevideo', 'zune', 'bingfinance', 'bingnews', 'bingsports', 'soundrecorder', 'bingweather', 'bing', 'onenote', 'windowsmaps', 'solitairecollection', 'solitaire', 'officehub', 'skypeapp', 'getstarted', '3dbuilder', 'drawboardpdf', 'freshpaint', 'nytcrossword', 'xboxapp', 'SurfaceHub', 'flipboard')  | % {
-    $app = Get-AppxPackage -AllUsers "*$_*"
-    if ($app) {
-      if ($app -is [array]) {
-        $app | % {
-          Remove-AppxPackage $_
-        }
-      } else  {
-        Remove-AppxPackage $app
-      }
-    }
-  }
+  $appNames = @('messaging', 'windowsalarms', 'windowscommunicationsapps', 'windowscamera', 'sway', 'phone', 'communicationsapps', 'people', 'zune', 'soundrecorder', 'bing', 'onenote', 'windowsmaps', 'solitaire', 'officehub', 'skypeapp', 'getstarted', '3dbuilder', 'drawboardpdf', 'freshpaint', 'nytcrossword', 'xboxapp', 'SurfaceHub', 'flipboard')
+  $appNames | % { Get-AppxPackage -AllUsers "*$_*" } | Remove-AppxPackage
 }
 
-Remove-WindowsApps -WhatIf
+Remove-WindowsApps
 
-function Restore-WindowsApps {
-  [CmdletBinding(SupportsShouldProcess=$True)]
-  param()
-    Get-AppxPackage -AllUsers | % {
-        Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"
-    }
-}
-
-
-Restore-WindowsApps -WhatIf
