72eed0b6d48747ee84b35b9c2178c93626dbed18
Robert Andersson <robert.andersson@offerta.se>
2017-10-06 15:57:17 +0200

    Restructure program names

--- powershell/scripts/removeMicrosoftDefaultPrograms.ps1
+++ powershell/scripts/removeMicrosoftDefaultPrograms.ps1
@@ -1,8 +1,8 @@
 [CmdletBinding(SupportsShouldProcess=$True)]
 param()
 function Remove-WindowsApps {
-  $appNames = @('messaging', 'windowsalarms', 'windowscommunicationsapps', 'windowscamera', 'sway', 'phone', 'communicationsapps', 'people', 'zune', 'soundrecorder', 'bing', 'onenote', 'windowsmaps', 'solitaire', 'officehub', 'skypeapp', 'getstarted', '3dbuilder', 'drawboardpdf', 'freshpaint', 'nytcrossword', 'xboxapp', 'xboxSpeech', 'xboxidentiy', 'xboxgameoverlay', 'SurfaceHub', 'flipboard')
-  $appNames | % { Get-AppxPackage -AllUsers "*$_*" } | Remove-AppxPackage
+  $appNames = @('Microsoft.Messaging', 'Microsoft.WindowsAlarms', 'Microsoft.windowscommunicationsapps', 'Microsoft.WindowsCamera', 'Microsoft.CommsPhone', 'Microsoft.windowscommunicationsapps', 'Microsoft.People', 'Microsoft.ZuneMusic', 'Microsoft.ZuneVideo', 'Microsoft.Office.OneNote', 'Microsoft.WindowsMaps', 'Microsoft.MicrosoftSolitaire', 'Microsoft.MicrosoftOfficeHub', 'Microsoft.SkypeApp', 'Microsoft.Getstarted', 'Microsoft.3DBuilder', 'Microsoft.XboxApp', 'Microsoft.XboxSpeech', 'Microsoft.XboxIdentityProvider', 'Microsoft.XboxGameOverlay', 'marchofempires', 'minecraft', 'sketchbook', 'Microsoft.Microsoft3DViewer', 'Microsoft.MicrosoftStickyNotes', 'Microsoft.BingWeather', 'Microsoft.Wallet', 'Microsoft.HoloShell', 'Microsoft.HoloCamera')
+  $appNames | % { Get-AppxPackage -AllUsers "$_*" } | Remove-AppxPackage
 }
 
 Remove-WindowsApps
