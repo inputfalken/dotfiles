3dcec018823da7f3859deccc294360473260bd91
Robert Andersson <robert.andersson@offerta.se>
2017-10-06 15:28:20 +0200

    Add xbox stuff which can be removed without errors...

--- powershell/scripts/removeMicrosoftDefaultPrograms.ps1
+++ powershell/scripts/removeMicrosoftDefaultPrograms.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding(SupportsShouldProcess=$True)]
 param()
 function Remove-WindowsApps {
-  $appNames = @('messaging', 'windowsalarms', 'windowscommunicationsapps', 'windowscamera', 'sway', 'phone', 'communicationsapps', 'people', 'zune', 'soundrecorder', 'bing', 'onenote', 'windowsmaps', 'solitaire', 'officehub', 'skypeapp', 'getstarted', '3dbuilder', 'drawboardpdf', 'freshpaint', 'nytcrossword', 'xboxapp', 'SurfaceHub', 'flipboard')
+  $appNames = @('messaging', 'windowsalarms', 'windowscommunicationsapps', 'windowscamera', 'sway', 'phone', 'communicationsapps', 'people', 'zune', 'soundrecorder', 'bing', 'onenote', 'windowsmaps', 'solitaire', 'officehub', 'skypeapp', 'getstarted', '3dbuilder', 'drawboardpdf', 'freshpaint', 'nytcrossword', 'xboxapp', 'xboxSpeech', 'xboxidentiy', 'xboxgameoverlay', 'SurfaceHub', 'flipboard')
   $appNames | % { Get-AppxPackage -AllUsers "*$_*" } | Remove-AppxPackage
 }
 
