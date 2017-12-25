68bc972794921076d075f2a8821108ba0d3aa91d
Robert Andersson <inputfalken@gmail.com>
2017-12-26 00:56:49 +0100

    Use named arguments and handle Save-GitConfig different

--- powershell/modules/dotfile-helper/dotfile-helper.psm1
+++ powershell/modules/dotfile-helper/dotfile-helper.psm1
@@ -1,20 +1,22 @@
 function Save-Dotfile {
   [CmdletBinding()]
   param (
-    [Parameter(Position=0, Mandatory=1)][System.IO.DirectoryInfo] $dotfilesDirectory,
-    [Parameter(Position=1, Mandatory=1)][string] $commitMessage,
-    [Parameter(Position=2, Mandatory=1)][bool] $pushRepo,
+    [Parameter(Position=0, Mandatory=1)][System.IO.DirectoryInfo] $dotfilesRepo,
+    [Parameter(Position=1, Mandatory=1)][string] $message,
+    [Parameter(Position=2, Mandatory=1)][bool] $push,
     [Parameter(Position=3, Mandatory=1)][System.IO.FileInfo] $sourceFile,
-    [Parameter(Position=4, Mandatory=1)][System.IO.FileInfo] $targetFile
+    [Parameter(Position=4, Mandatory=1)][System.IO.FileInfo] $targetFile,
+    [Parameter(Position=5, Mandatory=0)][ScriptBlock] $beforeCommit = {}
   )
 
-  if (Test-Path $dotfilesDirectory) {
+  if (Test-Path $dotfilesRepo) {
     if (Test-Path $sourceFile) {
-      Push-Location $dotfiles
+      Push-Location $dotfilesRepo
       Write-Host "Copying '$($sourceFile.FullName)' to '$($targetFile.FullName)'."
       Copy-Item $sourceFile $targetFile
-      git commit $targetFile -m $commitMessage
-      if ($pushRepo) { git push }
+      Invoke-Command $beforeCommit
+      git commit $targetFile -m $message
+      if ($push) { git push }
       Pop-Location
     } else {
       throw "Source file '$sourceFile' not found."
@@ -26,79 +28,86 @@ function Save-Dotfile {
 
 <#
   .SYNOPSIS
-  Saves the ConEmu config file to the dotfiles directory.
+  Saves the ConEmu config file to the dotfilesRepo directory.
 #>
 function Save-ConEmu {
   [CmdletBinding()]
   param (
-    [System.IO.DirectoryInfo] $dotfiles = "$HOME\dotfiles",
-    [string] $commitMessage = 'Update ConEmu settings',
-    [bool] $pushRepo = $false,
+    [System.IO.DirectoryInfo] $dotfilesRepo = "$HOME\dotfiles",
+    [string] $message = 'Update ConEmu settings',
+    [bool] $push = $false,
     [System.IO.FileInfo] $sourceFile = "$($env:APPDATA)\ConEmu.xml",
-    [System.IO.FileInfo] $targetFile = "$dotfiles\conemu\$($sourceFile.Name)"
+    [System.IO.FileInfo] $targetFile = "$dotfilesRepo\conemu\$($sourceFile.Name)"
   )
-  Save-Dotfile $dotfiles $commitMessage $pushRepo $sourceFile $targetFile
+  Save-Dotfile -DotfilesRepo $dotfilesRepo -Message $message -Push $push -SourceFile $sourceFile -TargetFile $targetFile
 }
 
 <#
   .SYNOPSIS
-  Saves the PowerShell profile ($PROFILE) file to the dotfiles directory.
+  Saves the PowerShell profile ($PROFILE) file to the dotfilesRepo directory.
 #>
 function Save-PowerShellProfile {
   [CmdletBinding()]
   param (
-    [System.IO.DirectoryInfo] $dotfiles = "$HOME\dotfiles",
-    [string] $commitMessage = 'Update PowerShell profile',
-    [bool] $pushRepo = $false,
+    [System.IO.DirectoryInfo] $dotfilesRepo = "$HOME\dotfiles",
+    [string] $message = 'Update PowerShell profile',
+    [bool] $push = $false,
     [System.IO.FileInfo] $sourceFile = $PROFILE,
-    [System.IO.FileInfo] $targetFile = "$dotfiles\powershell\$($PROFILE.Name)"
+    [System.IO.FileInfo] $targetFile = "$dotfilesRepo\powershell\$($PROFILE.Name)"
   )
-  Save-Dotfile $dotfiles $commitMessage $pushRepo $sourceFile $targetFile
+  Save-Dotfile -DotfilesRepo $dotfilesRepo -Message $message -Push $push -SourceFile $sourceFile -TargetFile $targetFile
 }
 
 <#
   .SYNOPSIS
-  Saves the GitConfig file to the dotfiles directory.
+  Saves the GitConfig file to the dotfilesRepo directory.
 #>
 function Save-GitConfig {
   [CmdletBinding()]
   param (
-    [System.IO.DirectoryInfo] $dotfiles = "$HOME\dotfiles",
-    [string] $commitMessage = 'Update GitConfig',
-    [bool] $pushRepo = $false,
-    [System.IO.FileInfo] $sourceFile = "$HOME\.GitConfig",
-    [System.IO.FileInfo] $targetFile = "$dotfiles\git\$($sourceFile.Name)"
+    [System.IO.DirectoryInfo] $dotfilesRepo = "$HOME\dotfiles",
+    [string] $message = 'Update GitConfig',
+    [bool] $push = $false,
+    [System.IO.FileInfo] $sourceFile = "$HOME\.gitconfig",
+    [System.IO.FileInfo] $targetFile = "$dotfilesRepo\git\$($sourceFile.Name)"
   )
-  Save-Dotfile $dotfiles $commitMessage $pushRepo $sourceFile $targetFile
-}
 
+  $name = git config --global user.name
+  $email = git config --global user.email
+  git config --global user.name 'Name'
+  git config --global user.email 'Email'
+  Save-Dotfile -DotfilesRepo $dotfilesRepo -Message $message -Push $push -SourceFile $sourceFile -TargetFile $targetFile -BeforeCommit {
+    git config --global user.name $name
+    git config --global user.email $email
+  }
+}
 
 <#
   .SYNOPSIS
-  Saves the .Vimrc file to the dotfiles directory.
+  Saves the .Vimrc file to the dotfilesRepo directory.
 #>
 function Save-VimConfig {
   [CmdletBinding()]
   param (
-    [System.IO.DirectoryInfo] $dotfiles = "$HOME\dotfiles",
-    [string] $commitMessage = 'Update Vim config',
-    [bool] $pushRepo = $false,
+    [System.IO.DirectoryInfo] $dotfilesRepo = "$HOME\dotfiles",
+    [string] $message = 'Update Vim config',
+    [bool] $push = $false,
     [System.IO.FileInfo] $sourceFile = "$HOME\.vimrc",
-    [System.IO.FileInfo] $targetFile = "$dotfiles\vim\$($sourceFile.Name)"
+    [System.IO.FileInfo] $targetFile = "$dotfilesRepo\vim\$($sourceFile.Name)"
   )
-  Save-Dotfile $dotfiles $commitMessage $pushRepo $sourceFile $targetFile
+  Save-Dotfile -DotfilesRepo $dotfilesRepo -Message $message -Push $push -SourceFile $sourceFile -TargetFile $targetFile
 }
 
 function Save-VimPluginsConfig {
   [CmdletBinding()]
   param (
-    [System.IO.DirectoryInfo] $dotfiles = "$HOME\dotfiles",
-    [string] $commitMessage = 'Update Vim plugins',
-    [bool] $pushRepo = $false,
+    [System.IO.DirectoryInfo] $dotfilesRepo = "$HOME\dotfiles",
+    [string] $message = 'Update Vim plugins',
+    [bool] $push = $false,
     [System.IO.FileInfo] $sourceFile = "$HOME\.vimrc.plugins",
-    [System.IO.FileInfo] $targetFile = "$dotfiles\vim\$($sourceFile.Name)"
+    [System.IO.FileInfo] $targetFile = "$dotfilesRepo\vim\$($sourceFile.Name)"
   )
-  Save-Dotfile $dotfiles $commitMessage $pushRepo $sourceFile $targetFile
+  Save-Dotfile -DotfilesRepo $dotfilesRepo -Message $message -Push $push -SourceFile $sourceFile -TargetFile $targetFile
 }
 
 $exportModuleMemberParams = @{
@@ -110,5 +119,4 @@ $exportModuleMemberParams = @{
         'Save-VimPluginsConfig'
     )
 }
-
 Export-ModuleMember @exportModuleMemberParams
