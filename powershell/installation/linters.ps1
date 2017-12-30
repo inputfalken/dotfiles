93d5c7b5eb52ca2cc368ca65fc1ff294a981d91f
Robert Andersson <inputfalken@gmail.com>
2017-12-31 00:38:44 +0100

    Add markdown lint installer

--- powershell/installation/linters.ps1
+++ powershell/installation/linters.ps1
@@ -8,6 +8,7 @@ function Setup-Linters {
   . $PSScriptRoot\utils.ps1
 
   function Install-Linter {
+    [CmdletBinding()]
     param (
       [Parameter(Mandatory=1)][string]$cmd,
       [Parameter(Mandatory=1)][ScriptBlock] $linterMissing,
@@ -19,50 +20,63 @@ function Setup-Linters {
   }
 
   function Install-JsonLint {
-    Install-Linter -CmdName JsonLint -LinterMissing {
+    [CmdletBinding()]
+    param ()
       Exec { npm install jsonlint -g }
-    }
   }
 
-  # TODO Add Markdown linter installation through gem
+  function Install-MardownLintTool {
+    Exec { gem install mdl } 
+  }
 
   function Install-XmlLint {
     [CmdletBinding()]
     param (
       [Parameter(Position=0,Mandatory=1)][string]$installationDirectory
     )
+    Push-Location $installationDirectory
 
-    Install-Linter -CmdName xmllint -LinterMissing {
-      Push-Location $installationDirectory
+    $downloads = @(
+      [PSCustomObject]@{ Uri='http://xmlsoft.org/sources/win32/iconv-1.9.2.win32.zip' ; FileName='iconv.zip' },
+      [PSCustomObject]@{ Uri='http://xmlsoft.org/sources/win32/libxml2-2.7.8.win32.zip' ; FileName='lbxml.zip' },
+      [PSCustomObject]@{ Uri='http://xmlsoft.org/sources/win32/libxmlsec-1.2.18.win32.zip' ; FileName='libxmlsec.zip' },
+      [PSCustomObject]@{ Uri='http://xmlsoft.org/sources/win32/zlib-1.2.5.win32.zip' ; FileName='zlib.zip' }
+    ) |
+      ForEach-Object { Invoke-WebRequest -Uri $_.Uri -OutFile $_.FileName ; $_ } |
+      Select-Object -ExpandProperty FileName | Get-Item |
+      ForEach-Object { Start-MpScan -ScanPath $_ -ScanType Custom -Verbose ; $_  } |
+      ForEach-Object { Expand-Archive -Path $_.FullName -DestinationPath $_.BaseName ; $_  } |
+      ForEach-Object { Remove-Item -Path $_.FullName ; Get-Item $_.BaseName }
 
-      $downloads = @(
-        [PSCustomObject]@{ Uri='http://xmlsoft.org/sources/win32/iconv-1.9.2.win32.zip' ; FileName='iconv.zip' },
-        [PSCustomObject]@{ Uri='http://xmlsoft.org/sources/win32/libxml2-2.7.8.win32.zip' ; FileName='lbxml.zip' },
-        [PSCustomObject]@{ Uri='http://xmlsoft.org/sources/win32/libxmlsec-1.2.18.win32.zip' ; FileName='libxmlsec.zip' },
-        [PSCustomObject]@{ Uri='http://xmlsoft.org/sources/win32/zlib-1.2.5.win32.zip' ; FileName='zlib.zip' }
-      ) |
-        ForEach-Object { Invoke-WebRequest -Uri $_.Uri -OutFile $_.FileName ; $_ } |
-        Select-Object -ExpandProperty FileName | Get-Item |
-        ForEach-Object { Start-MpScan -ScanPath $_ -ScanType Custom -Verbose ; $_  } |
-        ForEach-Object { Expand-Archive -Path $_.FullName -DestinationPath $_.BaseName ; $_  } |
-        ForEach-Object { Remove-Item -Path $_.FullName ; Get-Item $_.BaseName }
+    $downloads |
+      ForEach-Object { Get-ChildItem -Filter 'bin' -Recurse } |
+      ForEach-Object { Get-ChildItem $_.FullName } |
+      Select-Object -ExpandProperty FullName |
+      ForEach-Object { Move-Item -Path $_ -Destination . -Verbose }
 
-      $downloads |
-        ForEach-Object { Get-ChildItem -Filter 'bin' -Recurse } |
-        ForEach-Object { Get-ChildItem $_.FullName } |
-        Select-Object -ExpandProperty FullName |
-        ForEach-Object { Move-Item -Path $_ -Destination . -Verbose }
+    $downloads | Remove-Item -Force -Recurse -Verbose
 
-      $downloads | Remove-Item -Force -Recurse -Verbose
+    $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+    $location = (Get-Location).Path
+    $newpath = "$($oldpath)$($location);"
+    Set-ItemProperty -Path ‘Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment’ -Name PATH -Value $newPath
 
-      $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
-      $location = (Get-Location).Path
-      $newpath = "$($oldpath)$($location);"
-      Set-ItemProperty -Path ‘Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment’ -Name PATH -Value $newPath
-
-      Pop-Location
-    }
+    Pop-Location
   }
-  Install-XmlLint C:\tools\xml
-  Install-JsonLint
+
+  Install-Linter -CmdName xmllint -LinterMissing {
+    Install-XmlLint C:\tools\xml
+  }
+
+  Install-Linter -CmdName JsonLint -LinterMissing {
+    Install-JsonLint
+  }
+
+  Command-Exists -Cmd gem
+  -WhenExisting {
+    Install-Linter -CmdName mdl -LinterMissing { Install-MardownLintTool }
+  }
+  -WhenMissing {
+    Write-Host "Package manager 'Gem' does not exist"
+  }
 }
