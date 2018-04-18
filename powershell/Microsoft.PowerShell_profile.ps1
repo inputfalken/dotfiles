70ad7b0996c58c7f6fbb23e0e9754738ba8fb1d9
Robert Andersson <inputfalken@gmail.com>
2018-04-19 00:22:20 +0200

    Assign analyzer rules inside $PROFILE.

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -31,3 +31,49 @@ function gRootDirectory {
     else { Push-Location $location }
   } else { throw "'$(Get-Location)' is not a git directory/repository." }
 }
+
+$PowerShellAnalyzerRules = @{
+  IncludeRules = @(
+    'PSPlaceOpenBrace',
+    'PSPlaceCloseBrace',
+    'PSUseConsistentWhitespace',
+    'PSUseConsistentIndentation',
+    'PSAlignAssignmentStatement'
+  )
+
+  Rules        = @{
+    PSPlaceOpenBrace           = @{
+      Enable             = $true
+      OnSameLine         = $true
+      NewLineAfter       = $true
+      IgnoreOneLineBlock = $true
+    }
+
+    PSPlaceCloseBrace          = @{
+      Enable             = $true
+      NewLineAfter       = $false
+      IgnoreOneLineBlock = $true
+      NoEmptyLineBefore  = $false
+    }
+
+    PSUseConsistentIndentation = @{
+      Enable          = $true
+      Kind            = 'space'
+      IndentationSize = 2
+    }
+
+    PSUseConsistentWhitespace  = @{
+      Enable         = $true
+      CheckOpenBrace = $true
+      CheckOpenParen = $true
+      CheckOperator  = $true
+      CheckSeparator = $true
+    }
+
+    PSAlignAssignmentStatement = @{
+      Enable         = $true
+      CheckHashtable = $true
+    }
+  }
+}
+
