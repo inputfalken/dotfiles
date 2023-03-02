2410afc133e628a64872f4c889e865f028955a0f
Robert Andersson <robert.andersson@mathem.se>
2023-03-02 10:16:07 +0100

    Update powershell

--- powershell/modules/util-functions/util-functions.ps1
+++ powershell/modules/util-functions/util-functions.ps1
@@ -24,7 +24,8 @@ function When-Command {
 # This makes paths who contain spaces work with git.
 function Wrap-WithQuotes {
   $input `
-    | Foreach-Object { "'$_'" } `
+    | Foreach-Object {$_.Trim('"', "'")}
+    | Foreach-Object { "'$_'" }
     | Write-Output
 }
 
@@ -217,155 +218,10 @@ function Build-GitPullRequest {
 
 <#
 .SYNOPSIS
-  Helper function for executing command-line programs.
-.DESCRIPTION
-  This is a helper function that runs a scriptblock and checks the PS variable $lastexitcode to see if an error occcured.
-  If an error is detected then an exception is thrown.
-  This function allows you to run command-line programs without having to explicitly check the $lastexitcode variable.
-.PARAMETER cmd
-  The scriptblock to execute. This scriptblock will typically contain the command-line invocation.
-.PARAMETER errorMessage
-  The error message to display if the external command returned a non-zero exit code.
-.PARAMETER maxRetries
-  The maximum number of times to retry the command before failing.
-.PARAMETER retryTriggerErrorPattern
-  If the external command raises an exception, match the exception against this regex to determine if the command can be retried.
-  If a match is found, the command will be retried provided [maxRetries] has not been reached.
-.PARAMETER workingDirectory
-  The working directory to set before running the external command.
-.EXAMPLE
-  exec { svn info $repository_trunk } "Error executing SVN. Please verify SVN command-line client is installed"
-.LINK
-  https://raw.githubusercontent.com/psake/psake/master/src/public/Exec.ps1
-#>
-function Exec {
-  [CmdletBinding()]
-  param(
-    [Parameter(Mandatory = $true)] [scriptblock]$cmd,
-    [string]$errorMessage = ($msgs.error_bad_command -f $cmd),
-    [int]$maxRetries = 0,
-    [string]$retryTriggerErrorPattern = $null,
-    [string]$workingDirectory = $null
-  )
-
-  if ($workingDirectory) {
-    Push-Location -Path $workingDirectory
-  }
-
-  $tryCount = 1
-
-  do {
-    try {
-      $global:lastexitcode = 0
-      & $cmd
-      if ($lastexitcode -ne 0) {
-        throw "Exec: $errorMessage"
-      }
-      break
-    } catch [exception] {
-      if ($tryCount -gt $maxRetries) {
-        throw $_
-      }
-
-      if ($retryTriggerErrorPattern -ne $null) {
-        $isMatch = [regex]::IsMatch($_.Exception.Message, $retryTriggerErrorPattern)
-
-        if ($isMatch -eq $false) {
-          throw $_
-        }
-      }
-
-      "Try $tryCount failed, retrying again in 1 second..."
-
-      $tryCount++
-
-      [System.Threading.Thread]::Sleep([System.TimeSpan]::FromSeconds(1))
-    } finally {
-      if ($workingDirectory) {
-        Pop-Location
-      }
-    }
-  }
-  while ($true)
-}
-
-<#
-.SYNOPSIS
   Reloads the System Envrionmental variable PATH
 #>
-function Reload-Path {
+function Update-Path {
   $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-}
-
-<#
-.SYNOPSIS
-  Lists choco packages name and version.
-  Returns an array of PSCustomObject with properties:
-  * Package
-  * Version
-#>
-function Get-ChocolateyPackages {
-  Write-Host 'Getting installed packages' -ForegroundColor Yellow
-  # Gets the packages names
-  $packages = choco list --local-only
-  if ($packages) {
-    # Get rid of redundant info,
-    $Packages `
-      | Take-While { $args[0] -notmatch '\d\spackages\sinstalled\.' } `
-      | ForEach-Object `
-      -Process {
-      $split = $_.split(' ')
-      [pscustomobject]@{ Package = $split[0]; Version = $split[1] }
-    } `
-      | Sort-Object Package
-  } else {
-    # Return empty array if $packages is null
-    @()
-  }
-}
-
-function Find-SolutionFile {
-  param(
-    [Parameter(Position = 1, Mandatory = 1)] [string]$Path,
-    [Parameter(Position = 1, Mandatory = 1)] [scriptblock]$OnSuccess
-  )
-
-  function Is-SolutionFile {
-    param([Parameter(Position = 1, Mandatory = 1)] [System.IO.FileSystemInfo]$File )
-    $File.Extension -eq '.sln'
-  }
-
-  $SolutionPath = Get-Item $Path -ErrorAction Stop
-
-  if (Test-Path $SolutionPath -Type Leaf) {
-    if (Is-SolutionFile $SolutionPath) { Invoke-Command -ScriptBlock $OnSuccess -ArgumentList $SolutionPath }
-    else { throw "File '$SolutionPath' is not not a solution file." }
-  } else {
-    $SolutionFiles = $SolutionPath `
-      | Get-ChildItem `
-      | Where-Object { Is-SolutionFile $_ }
-    if (($SolutionFiles.Count -eq 1) -and ($SolutionPath.FullName -eq (Get-Location).Path)) { Invoke-Command -ScriptBlock $OnSuccess -ArgumentList $SolutionFiles[0] }
-    elseif ($SolutionFiles.Count -gt 1) { throw "You need to specify one of the following: $($SolutionFiles -join ', ')." }
-    else { throw "No solution file found with path '$SolutionPath'." }
-  }
-}
-
-function Dotnet-GetSolutionProjects {
-  [OutputType('System.IO.FileSystemInfo')]
-  [CmdletBinding()]
-  param(
-    [Parameter(Position = 0, Mandatory = 0)] [string]$SolutionPath = '.\'
-  )
-  Find-SolutionFile -Path $SolutionPath -OnSuccess {
-    param(
-      $SolutionFile
-    )
-    dotnet sln $SolutionFile list `
-      | Where-Object { Test-Path $_ -Type Leaf } `
-      | Get-Item `
-      | Write-Output
-    Write-Host "`r`n`r`nListed projects found in '$($SolutionFile.FullName)'."
-  }
 }
 
 <#
@@ -518,7 +374,7 @@ function Clear-DotnetProject {
 function Start-Nvim {
   $terminalSession = [bool]$env:NVIM_LISTEN_ADDRESS
   # If a terminal is used within neovim, send the files to the neovim hosting the terminal rather than creating a nested neovim process.
-  $neovim = if ($terminalSession) { 'C:\Python36\Scripts\nvr.exe' } else { 'C:\tools\neovim\Neovim\bin\nvim.exe' }
+  $neovim = if ($terminalSession) { 'nvr' } else { 'nvim' }
   $expression = if ($args[0] -eq '-') {
     $path = Join-Path -Path $env:TEMP 'piped-nvim-argument.txt'
     $input `
@@ -699,16 +555,6 @@ function gRootDirectory {
   } else { throw "Function 'gRootDirectory' requires 'git' to be globally available." }
 }
 
-function gadd {
-  param([string] $Filter = '*')
-  Test-GitRepository
-  $arguments = $input + $args `
-    | Where-Object { Smart-Filter -Filter $Filter -Text $_ } `
-    | Wrap-WithQuotes
-  if ($arguments.Count -gt 0) { "git add $($arguments -join ' ')" | Invoke-Expression }
-  else { Write-Output 'No arguments supplied.' }
-}
-
 function gcheckout {
   param(
       [Parameter(ValueFromPipeline, Mandatory = 0)][ValidateNotNull()] $InputObject,
@@ -773,7 +619,7 @@ function glistFiles {
     $concatenatedArguments = ($Arguments += $InputObject) | Where-Object { [string]::IsNullOrWhiteSpace($_) -eq $false }
     $joinedArgs = if ($Arguments.Count -gt 0) { if ($Strict) { $concatenatedArguments | Wrap-WithQuotes } else { $concatenatedArguments | ForEach-Object { "*$_*" } | Wrap-WithQuotes } } else { [string]::Empty }
 
-    $expression = if ([string]::IsNullOrWhiteSpace($joinedArgs)) { "git ls-files *" } else { "git ls-files " + $joinedArgs }
+    $expression = if ([string]::IsNullOrWhiteSpace($joinedArgs)) { "git -c core.quotepath=false ls-files *" } else { "git  -c core.quotepath=false ls-files " + $joinedArgs }
     $expression  `
       | Invoke-Expression `
       | Get-Item
@@ -829,145 +675,4 @@ function gdiffVim {
 
 function Edit-Profile {
   @( $PROFILE, '-c lcd %:h') | Start-Nvim
-}
-
-function Get-ClipboardText {
-
-  [CmdletBinding()] # to support -OutVariable and -Verbose
-  param()
-
-  if ($PSVersionTable.PSEdition -eq 'Desktop') {
-    # *Windows* PowerShell
-    if ($PSVersionTable.PSVersion -ge [version] '5.1.0') {
-      # Ps*Win* v5.1+ now has Get-Clipboard / Set-Clipboard cmdlets.
-      Get-Clipboard -Format Text
-    } else {
-      Add-Type -AssemblyName System.Windows.Forms
-      if ([threading.thread]::CurrentThread.ApartmentState.ToString() -eq 'STA') {
-        # -- STA mode:
-        Write-Verbose "STA mode: Using [Windows.Forms.Clipboard] directly."
-        # To be safe, we explicitly specify that Unicode (UTF-16) be used - older platforms may default to ANSI.
-        [System.Windows.Forms.Clipboard]::GetText([System.Windows.Forms.TextDataFormat]::UnicodeText)
-      } else {
-        # -- MTA mode: Since the clipboard must be accessed in STA mode, we use a [System.Windows.Forms.TextBox] instance to mediate.
-        Write-Verbose "MTA mode: Using a [System.Windows.Forms.TextBox] instance for clipboard access."
-        $tb = New-Object System.Windows.Forms.TextBox
-        $tb.Multiline = $tru
-        $tb.Paste()
-        $tb.Text
-      }
-    }
-  } else {
-    # PowerShell Core
-    if ($env:OS -eq 'Windows_NT') {
-      # Gratefully adapted from http://stackoverflow.com/a/15747067/45375
-      # Note that trying the following directly from PowerShell Core does NOT work,
-      #   (New-Object -ComObject htmlfile).parentWindow.clipboardData.getData('text')
-      # because .parentWindow is always $null
-      $tempFile = [io.path]::GetTempFileName()
-      "WSH.Echo(WSH.CreateObject('htmlfile').parentWindow.clipboardData.getData('text'));" | set-content $tempFile
-      cscript /nologo /e:JScript $tempFile
-      Remove-Item $tempFile
-    } elseif ((uname) -eq 'Darwin') {
-      pbpaste
-    } else {
-      # Note: May work on Ubuntu only, and there only if xclip was
-      # installed with: sudo apt install xclip
-      xclip -sel clipboard -o
-    }
-  }
-}
-
-function Set-ClipboardText() {
-  [CmdletBinding()]
-  Param(
-    [Parameter(Mandatory, ValueFromPipeline)]
-    [ValidateNotNull()]
-    $InputObject
-  )
-
-  # Each input object is converted to a string representation with Out-String,
-  # unless it already is a string; the representations of multiple input objects
-  # are separated - but not terminated - with a platform-native newline.
-  $allText = $sep = ''
-  # $Input having a value means that pipeline input was provided.
-  #     Note: Since we want to access all pipeline input *at once*,
-  #           we do NOT use a process {} block.
-  if ($Input) { $InputObject = $Input }
-  foreach ($o in $InputObject) {
-    $text = if ($o -is [string]) { $o } else { $o | Out-String }
-    $allText += $sep + $text
-    if (-not $sep) { $sep = [Environment]::NewLine }
-  }
-
-  if ($PSVersionTable.PSEdition -eq 'Desktop') {
-    # *Windows* PowerShell
-
-    if ($PSVersionTable.PSVersion -ge [version] '5.1.0') {
-      # Ps*Win* v5.1+ now has Get-Clipboard / Set-Clipboard cmdlets.
-      Set-Clipboard -Value $allText
-    } else {
-      Add-Type -AssemblyName System.Windows.Forms
-      if ([threading.thread]::CurrentThread.ApartmentState.ToString() -eq 'STA') {
-        # -- STA mode: we can use [Windows.Forms.Clipboard] directly.
-        Write-Verbose "STA mode: Using [Windows.Forms.Clipboard] directly."
-        if ($allText.Length -eq 0) { $AllText = "`0" } # Strangely, SetText() breaks with an empty string, claiming $null was passed -> use a null char.
-        # To be safe, we explicitly specify that Unicode (UTF-16) be used - older platforms may default to ANSI.
-        [System.Windows.Forms.Clipboard]::SetText($allText, [System.Windows.Forms.TextDataFormat]::UnicodeText)
-
-      } else {
-        # -- MTA mode: Since the clipboard must be accessed in STA mode, we use a [System.Windows.Forms.TextBox] instance to mediate.
-        Write-Verbose "MTA mode: Using a [System.Windows.Forms.TextBox] instance for clipboard access."
-        if ($allText.Length -eq 0) {
-          # !! This approach cannot set the clipboard to an empty string: the text box must
-          # !! must be *non-empty* in order to copy something. A null character doesn't work.
-          # !! We use the least obtrusive alternative - a newline - and issue a warning.
-          $allText = "`r`n"
-          Write-Warning "Setting clipboard to empty string not supported in MTA mode; using newline instead."
-        }
-        $tb = New-Object System.Windows.Forms.TextBox
-        $tb.Multiline = $true
-        $tb.Text = $allText
-        $tb.SelectAll()
-        $tb.Copy()
-      }
-    }
-  } else {
-    # PowerShell *Core*
-
-    # No native PS support for writing to the clipboard ->
-    # external utilities must be used.
-
-    # To prevent adding a trailing \n, which PS inevitably adds when sending
-    # a string through the pipeline to an external command, use a temp. file,
-    # whose content can be provided via native input redirection (<)
-    $tmpFile = [io.path]::GetTempFileName()
-
-    # Determine the encoding: Unix platforms need UTF8, whereas
-    # Windows's clip.exe needs UTF-16LE - both in *BOM-less* form.
-    if ($env:OS -eq 'Windows_NT') {
-      # clip.exe on Windows only works as expected with non-ASCII characters
-      # with UTF-16LE encoding.
-      # Unfortunately, it invariably treats the BOM as *data* too, so
-      # we cannot use 'Set-Content -Enocding Unicode' and must use a
-      # BOM-less encoding via the .NET Framework.
-      [io.file]::WriteAllText($tmpFile, $allText, [System.Text.UnicodeEncoding]::new($false, $false))
-    } else {
-      # PowerShell's UTF8 encoding invariably creates a file WITH BOM
-      # so we use the .NET Framework, whose default is BOM-*less* UTF8.
-      [IO.File]::WriteAllText($tmpFile, $allText)
-    }
-
-    if ($env:OS -eq 'Windows_NT') {
-      Write-Verbose "Windows: using clip.exe"
-      cmd /c clip.exe '<' $tmpFile
-    } elseif ((uname) -eq 'Darwin') {
-      Write-Verbose "macOS: Using pbcopy."
-      bash -c "pbcopy < '$tmpFile'"
-    } else {
-      Write-Verbose "Linux: trying xclip -sel clip"
-      bash -c "xclip -sel clip < '$tmpFile'"
-    }
-    Remove-Item $tmpFile
-  }
 }
--- powershell/modules/util-functions/util-functions.psm1
+++ powershell/modules/util-functions/util-functions.psm1
@@ -2,30 +2,23 @@
 
 $exportModuleMemberParams = @{
   Function = @(
-    'Clear-DotnetProject',
-    'Dotnet-AddProjectToSolution',
-    'Dotnet-GetSolutionProjects',
-    'Edit-Profile',
-    'Exec',
-    'gdiffFiles',
-    'gdiffVim',
-    'Get-ChocolateyPackages',
-    'guntrackedFiles',
-    'gbranches',
     'Build-GitPullRequest',
-    'gignoredFiles',
-    'Start-Nvim',
-    'Reload-Path',
-    'Tail-File',
-    'Get-ClipboardText',
-    'Set-ClipboardText',
-    'glistFiles',
-    'gadd',
-    'gcheckout',
-    'gdiffFilesCheckout',
+    'Clear-DotnetProject',
+    'Edit-Profile',
     'Exclude-Object',
     'Include-Object',
     'Skip-Object'
+    'Start-Nvim',
+    'Update-Path',
+    'gRootDirectory',
+    'gbranches',
+    'gcheckout',
+    'gdiffFiles',
+    'gdiffFilesCheckout',
+    'gdiffVim',
+    'gignoredFiles',
+    'glistFiles',
+    'guntrackedFiles'
   )
 }
 Export-ModuleMember @exportModuleMemberParams
