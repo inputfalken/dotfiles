e12f54499069be32da098ba303b9d84019c48bbd
Robert Andersson <inputfalken@gmail.com>
2024-05-22 18:47:12 +0200

    Improve reusability

--- vim/lua/plugins/debugger/csharp.lua
+++ vim/lua/plugins/debugger/csharp.lua
@@ -1,86 +1,68 @@
-local prompt_selection = function(prompt_title, options, optionFormatter)
+local prompt_selection = function(items, opts)
   local utils = require('modules.util');
-  local mapped_options = optionFormatter == nil
-      and utils.map(options, function(i, x) return string.format('%s : %s', i, x) end)
-      or utils.map(options, function(i, x) return string.format('%s : %s', i, optionFormatter(x)) end)
+  local mapped_options = opts.element_stringifier == nil
+      and utils.map(items, function(i, x) return string.format('%s : %s', i, x) end)
+      or utils.map(items, function(i, x) return string.format('%s : %s', i, opts.element_stringifier(x)) end)
 
-  table.insert(mapped_options, 1, prompt_title)
+  table.insert(mapped_options, 1, opts.multiple_title_message)
   local index = vim.fn.inputlist(mapped_options)
 
   return index > 0
-      and options[index]
+      and items[index]
       or nil
 end
 
-local resolve_path = function(cmd, opts)
-  local results = vim.fn.systemlist(cmd)
-
-  if #results == 0 then
-    print(string.format('Command "%s" gave no result', cmd))
+local select_element_from_table = function(items, opts)
+  if #items == 0 then
+    print(string.format('Command "%s" gave no result', items))
     return
   end
 
-  return #results > 1
-      and prompt_selection(opts.multiple_title_message, results)
-      or results[1]
+  return (opts.smartSelect and #items == 1)
+      and items[1]
+      or prompt_selection(items, opts)
 end
 
 
-local dll_selection = function(path, searchInward)
-  local project_file = resolve_path(
-    string.format(
-      searchInward
-      and
-      [[
-        Get-ChildItem -Recurse -Depth 10 -Path '%s' -File -Filter '*.csproj' `
-        | Select-Object -ExpandProperty FullName
-      ]]
-      or
-      [[
-        $items
-        $cwd = Get-Item -Path '%s'
-        $rootDirectory = Get-Item -Path '/'
-        do {
-          $items = Get-ChildItem -Path $cwd.FullName -Filter '*.csproj'
-          $cwd = $cwd.Parent
-        } until ($items -or ($cwd.FullName -eq $rootDirectory.FullName))
-        $items | Select-Object -ExpandProperty FullName
-      ]],
-      path
-    ),
-    {
-      empty_message = 'No csproj files found in ' .. path,
-      multiple_title_message = 'Select project:'
-    })
-  if project_file == nil then
+local dll_selection = function(project_list_command)
+  local project_json = vim.fn.system(project_list_command)
+
+  local project_file_path = select_element_from_table(
+    vim.json.decode(project_json),
+    { multiple_title_message = 'Select project:', smartSelect = true }
+  )
+
+  if project_file_path == nil then
     return
   end
 
-  local project_directory = vim.fn.fnamemodify(project_file, ':p:h')
-  local bin_path = resolve_path(
+  local project_directory_path = vim.fn.fnamemodify(project_file_path, ':p:h')
+  local binary_paths_json = vim.fn.system(
     string.format(
       [[
         Get-ChildItem -Path %s -Recurse -Directory -Filter 'bin' `
         | Get-ChildItem -Filter 'Debug' `
         | Get-ChildItem `
-        | Select-Object -ExpandProperty FullName
+        | Select-Object -ExpandProperty FullName `
+        | ConvertTo-Json -Compress -AsArray
       ]],
-      project_directory
-    ),
-    {
-      empty_message = 'No dotnet directories found in the "bin" directory. Ensure project has been built.',
-      multiple_title_message = 'Select NET Version:'
-    }):match('^%s*(.-)%s*$')
-  if bin_path == nil then
+      project_directory_path
+    )
+  )
+  local binary_path = select_element_from_table(
+    vim.json.decode(binary_paths_json),
+    { multiple_title_message = 'Select binary:', smartSelect = true }
+  )
+  if binary_path == nil then
     return
   end
 
   -- Sets the working directory for the window/buffer only.
-  vim.cmd(string.format('lcd %s', project_directory))
+  vim.cmd(string.format('lcd %s', project_directory_path))
 
-  return bin_path
+  return binary_path
       .. '/'
-      .. vim.fn.fnamemodify(project_file, ':t:r') -- name of file
+      .. vim.fn.fnamemodify(project_file_path, ':t:r') -- name of file
       .. '.dll'
 end
 
@@ -101,7 +83,16 @@ return {
         name    = 'Launch (CWD)',
         request = 'launch',
         program = function()
-          return dll_selection(vim.fn.getcwd(), true) or dap.ABORT
+          return dll_selection(
+            string.format(
+              [[
+                Get-ChildItem -Recurse -Depth 10 -Path '%s' -File -Filter '*.csproj' `
+                | Select-Object -ExpandProperty FullName `
+                | ConvertTo-Json -Compress -AsArray
+              ]],
+              vim.fn.getcwd()
+            )
+          ) or dap.ABORT
         end,
       },
       {
@@ -109,7 +100,23 @@ return {
         name    = 'Launch (CWF)',
         request = 'launch',
         program = function()
-          return dll_selection(vim.fn.expand('%:p:h'), false) or dap.ABORT
+          return dll_selection(
+            string.format(
+              [[
+                $items
+                $cwd = Get-Item -Path '%s'
+                $rootDirectory = Get-Item -Path '/'
+                do {
+                  $items = Get-ChildItem -Path $cwd.FullName -Filter '*.csproj'
+                  $cwd = $cwd.Parent
+                } until ($items -or ($cwd.FullName -eq $rootDirectory.FullName))
+                $items `
+                | Select-Object -ExpandProperty FullName `
+                | ConvertTo-Json -Compress -AsArray
+              ]],
+              vim.fn.expand('%:p:h')
+            )
+          ) or dap.ABORT
         end,
       },
       {
@@ -119,12 +126,12 @@ return {
         processId = function()
           local name_id_json = vim.fn.system(
             [[
-               Get-Process
-               | Where-Object { $_.Path -ne $null } `
-               | Where-Object { $_.Path.StartsWith($env:USERPROFILE) } `
-               | Sort-Object -Descending StartTime `
-               | Select-Object Id, @{Name = 'Name'; Expression='ProcessName'} `
-               | ConvertTo-Json -Compress
+              Get-Process
+              | Where-Object { $_.Path -ne $null } `
+              | Where-Object { $_.Path.StartsWith($env:USERPROFILE) } `
+              | Sort-Object -Descending StartTime `
+              | Select-Object Id, @{Name = 'Name'; Expression='ProcessName'} `
+              | ConvertTo-Json -Compress -AsArray
             ]]
           )
           local table = vim.json.decode(name_id_json);
@@ -132,23 +139,24 @@ return {
             print('Could not convert system call into json.')
             return dap.ABORT
           end
-          if (#table == 0) then
-            print('No processes found')
-            return dap.ABORT
-          end
-          local decision = prompt_selection('Select process', table,
-            function(x)
-              if (x.Name == nil or x.Id == nil) then
-                error(string.format('Unexpected fields in JSON \'%s\''), name_id_json)
-              end
-              return string.format('%s(%s)', x.Name, x.Id)
-            end
-          )
-          if (decision == nil) then
-            return dap.ABORT
-          end
 
-          return decision.Id
+          local process = select_element_from_table(
+            table,
+            {
+              multiple_title_message = 'Select process:',
+              smartSelect = false,
+              element_stringifier = function(x)
+                if (x.Name == nil or x.Id == nil) then
+                  error(string.format('Unexpected fields in JSON \'%s\''), name_id_json)
+                end
+                return string.format('%s(%s)', x.Name, x.Id)
+              end
+            }
+          )
+
+          return process == nil
+              and dap.ABORT
+              or process.Id
         end,
         args      = {},
       },
